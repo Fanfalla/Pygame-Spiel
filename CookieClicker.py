f781a37c852deaef9a26e0f650a9034306c96d5d
Fanfalla <116183346+Fanfalla@users.noreply.github.com>
2022-11-03 17:12:08 +0100

    Update CookieClicker.py

--- CookieClicker.py
+++ CookieClicker.py
@@ -13,7 +13,7 @@ def main():
     #Cookie
     cookie = pygame.image.load("Cookie.jpg")
     cookie = pygame.transform.scale(cookie, (100,100))
-    screen.blit(cookie, (0, 0))
+    screen.blit(cookie, (300, 300))
     
     Cookies = 0
     #Button
